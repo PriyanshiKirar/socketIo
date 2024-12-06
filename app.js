f0a7a32b833d8be5a0cad074d0f49e7b48a7e3c9
Priyanshi Kirar <patelpriyanshi866@gmail.com>
2024-12-06 20:53:30 +0530

    6 dec

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 io.on('connection', socket => {
   console.log("connect to  socketio");
-
+console.log(socket.id)
   // secont step
   // server p mssage recieve krna on meansn resieve
   socket.on("message", msg => {
