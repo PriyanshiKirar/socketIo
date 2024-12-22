13ebf046c6caebcbfce912b0d4ada6b74822f5c4
Priyanshi Kirar <patelpriyanshi866@gmail.com>
2024-12-22 20:57:33 +0530

    22

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose=require("mongoose");
 
 async function connecteddb(){
     await mongoose.connect("mongodb://127.0.0.1:27017/sockett");
-    console.log("conectedToDb work properly");
+    console.log("conectedToDb for working");
 }
 connecteddb();
 module.exports=mongoose.connection;
